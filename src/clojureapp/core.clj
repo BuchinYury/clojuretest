d063b106aa4abcf0a0f85f5cc9bb046591632d1c
Buchin Iurii <IBuchin@bell-main.bellintegrator.ru>
2017-06-23 18:08:04 +0300

    init commit

---
+++ src/clojureapp/core.clj
@@ -0,0 +1,14 @@
+(ns clojureapp.core)
+
+(defn -main [& args]
+    (println "Hello, World!"))
+
+(defn m [max]
+    (println "Hello" max))
+
+(defn m2 [max]
+    (println max))
+
+(defn m3 [max]
+    (+ max max))
+
